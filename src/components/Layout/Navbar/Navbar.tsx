5e35012a19c6e6736be6be9970cca001045a12a1
Nahuel Bonader <bonadernahuel@gmail.com>
2023-03-03 13:45:34 -0300

    update navbar and categories

--- src/components/Layout/Navbar/Navbar.tsx
+++ src/components/Layout/Navbar/Navbar.tsx
@@ -31,7 +31,7 @@ const Navbar = ({}) => {
         <figure className={styles.logo}>
           <Image src={Icons.LOGO} alt="Renuver Logo" layout="fill" />
         </figure>
-        {/* <Search
+        <Search
           name="searchdesktop"
           variant="secundary"
           placeholder="Buscá por modelos"
@@ -39,25 +39,26 @@ const Navbar = ({}) => {
         <figure className={styles.login}>
           <Image src={Icons.LOGIN} alt="login Logo" layout="fill" />
         </figure>
-        <div className={`${styles.burgerContainer} ${isOpenMenu ? styles.openMenu : ""}`}>
-          <button
-            onClick={handleClick}
-            className={`${styles.burger}`}
-          >
+        <div
+          className={`${styles.burgerContainer} ${
+            isOpenMenu ? styles.openMenu : ""
+          }`}
+        >
+          <button onClick={handleClick} className={`${styles.burger}`}>
             <div></div>
             <div></div>
             <div></div>
           </button>
         </div>
-        
-        <Menu isOpen={isOpenMenu} handleClick={handleClick} /> */}
+
+        <Menu isOpen={isOpenMenu} handleClick={handleClick} />
       </div>
       <nav className={styles.section2}>
-        {/* <Search
+        <Search
           name="searchMobile"
           variant="primary"
           placeholder="Buscá por modelos"
-        /> */}
+        />
       </nav>
     </header>
   );
